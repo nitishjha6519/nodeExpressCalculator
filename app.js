dab432c3e5b5c98c78ef3ea15458606b90fdfb08
Nitish Jha <nitishjha6519@gmail.com>
2021-12-19 17:30:11 +0530

    added root route / with callback so we know its working

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express= require("express");
 const app= express();
 
+app.get("/", function(req,res){
+console.log(req);
+});
 
 app.listen(3000, function(){
     console.log("server is running at port 3000");
