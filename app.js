5a22be2c39e57174b9ccc2c095cd58ec3adf82d7
Nitish Jha <nitishjha6519@gmail.com>
2021-12-19 17:42:43 +0530

    create multiple pages other than home

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", function(req,res){
 res.send("Hello world");
 });
 
+app.get("/contact", function(req,res){
+res.send("<h1>You can contact me at nitishjha6519@gmail.com</h1>");
+});
+
+app.get("/about", function(req,res){
+    res.send("Hello, my name is nitish and i love to create things");
+})
+
 app.listen(3000, function(){
     console.log("server is running at port 3000");
 });
