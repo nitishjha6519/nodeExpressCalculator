f7cd3cca91c4228affd4e91f0a663780a3894652
Nitish Jha <nitishjha6519@gmail.com>
2021-12-19 20:27:38 +0530

    post data + access form data

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express= require("express");
 const app= express();
+const bodyParser=require("body-parser");
+
+
 
 app.get("/", function(req,res){
  res.sendFile(__dirname + "/index.html");
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+//form data can be accessed through urlencded//
+
+app.post("/", function(req,res){
+    console.log(req.body);
+    // pased data is in req.body//
+
+    var num1=Number(req.body.num1);
+    var num2=Number(req.body.num2);
+    var result=num1+num2;
+    console.log(result);
+ res.send("The addition of number is " + result);
 });
 
 app.listen(3000, function(){
